fc19de97bd911fa5f74216a2e6978702df90cbaf
Ekansh0110 <ekanshbansal0501@gmail.com>
2021-08-11 15:41:32 +0530

    first commit

---
+++ app2.js
@@ -0,0 +1,21 @@
+const ratingsEl = document.querySelectorAll(".rating");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
+
+ratingsEl.forEach((el) => {
+  el.addEventListener("click", () => {
+    ratingsEl.forEach((innerEl) => {
+      innerEl.classList.remove("active");
+    });
+
+    el.classList.add("active");
+  });
+});
+
+sendBtn.addEventListener("click", () => {
+  panel.innerHTML = `
+		<i class="fas fa-heart"></i>
+		<strong>Thank you, for the review!</strong>
+		<p>We'll use your feedback in improve our games.</p>
+	`;
+});
