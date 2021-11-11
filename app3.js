bb45972f83f8d9a914e85242bc96783da3404b9d
Achintya Wadhwa <69971284+Achintya312@users.noreply.github.com>
2021-11-11 11:12:20 +0530

    Update app3.js

--- app3.js
+++ app3.js
@@ -1,1 +1,11 @@
+const http = require('http')
+const fs = require('fs')
+const fileContent = fs.readFileSync('index2.html')
 
+const server = http.createServer((req, res)=>{
+  res.writeHead(200, {'Content-type':'text/html});
+  res.end(fileContent)
+})
+
+server.listen(80, '127.0.0.1', ()=>{
+  console.log("listening on port 80")
